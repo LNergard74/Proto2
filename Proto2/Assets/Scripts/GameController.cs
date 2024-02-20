72e1b4dd3826348b5bc364ce6b078baa8f416d02
RiderH <114954577+RiderH@users.noreply.github.com>
2024-02-20 14:17:56 -0600

    MainMenu

---
+++ Proto2/Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject credits;
+
+    public bool isPaused = false;
+    private bool creditsToggle = false;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void CreditsToggle()
+    {
+        creditsToggle = !creditsToggle;
+        credits.SetActive(creditsToggle);
+    }
+    public void Resume()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void Pause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pauseMenu.SetActive(false);
+        }
+    }
+}
